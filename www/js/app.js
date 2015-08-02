e4270f7045c7b4b75bff7c4b0c541bf52bd49fbb
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-02 13:14:04 -0700

    add materialized toolbar, improved account page list

--- www/js/app.js
+++ www/js/app.js
@@ -29,7 +29,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','n
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
-  $mdThemingProvider.theme('default').primaryPalette('purple').accentPalette('indigo');
+  $mdThemingProvider.theme('default');
   $mdGestureProvider.skipClickHijack();
   $stateProvider
 
